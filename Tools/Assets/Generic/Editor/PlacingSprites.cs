5716caa75ffd519d3db738b743aaf41be1841cba
Jeff Harper <jeff.david.harper@gmail.com>
2019-03-04 23:24:39 -0600

    Placing Sprites
    Use a sprites rect to place it in world space.

---
+++ Tools/Assets/Generic/Editor/PlacingSprites.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PlacingSprites : Editor
+{
+    [MenuItem("Tools/Place Sprites")]
+    public static void Place()
+    {
+        Object[] objs = Selection.objects;
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].GetType() == typeof(Texture2D))
+            {
+                Texture2D tex = (Texture2D)objs[i];
+                string path = AssetDatabase.GetAssetPath(tex);
+                Object[] sprites = AssetDatabase.LoadAllAssetsAtPath(path);
+                TextureImporter importer = (TextureImporter)TextureImporter.GetAtPath(path);
+                float pixels = importer.spritePixelsPerUnit;
+                GameObject parent = new GameObject(tex.name);
+                for (int j = 0; j < sprites.Length; j++)
+                {
+                    if (sprites[j].GetType() == typeof(Sprite))
+                    {
+                        Sprite sprite = (Sprite)sprites[j];
+                        GameObject go = new GameObject(sprite.name);
+                        go.transform.parent = parent.transform;
+                        SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+                        sr.sprite = sprite;
+                        Rect rect = sprite.rect;
+                        sr.transform.position = new Vector2((rect.x / pixels) - ((tex.width / 2) / pixels) + rect.width / pixels / 2,
+                            (rect.y / pixels) - ((tex.height / 2) / pixels) + rect.height / pixels / 2);
+                    }
+                }
+            }
+        }
+    }
+}
