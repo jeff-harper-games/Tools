908b336b052d55059c27a792d9cfe1d6ca434bc0
Jeff Harper <jeff.david.harper@gmail.com>
2019-05-21 07:25:38 -0500

    BuildMesh
    Create a 3D Mesh from a PolyCollider.

---
+++ Tools/Assets/BuildMesh/BuildMesh.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class BuildMesh
+{
+    public static void Build(GameObject container, PolygonCollider2D poly)
+    {
+        List<Mesh> meshes = new List<Mesh>();
+
+        for (int i = 0; i < poly.pathCount; i++)
+        {
+            Vector2[] verts = poly.GetPath(i);
+
+            // http://wiki.unity3d.com/index.php/Triangulator
+            Triangulator tr = new Triangulator(verts);
+            int[] indices = tr.Triangulate();
+
+            Vector3[] vertices = new Vector3[verts.Length];
+            for (int j = 0; j < verts.Length; j++)
+            {
+                vertices[j] = new Vector3(verts[j].x, verts[j].y, 0);
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.triangles = indices;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            meshes.Add(mesh);
+        }
+
+        CombineInstance[] combine = new CombineInstance[meshes.Count];
+        for (int i = 0; i < combine.Length; i++)
+        {
+            combine[i].mesh = meshes[i];
+            combine[i].transform = container.transform.localToWorldMatrix;
+        }
+        
+        
+        MeshRenderer mr = container.GetComponent<MeshRenderer>();
+        if (!mr)
+            mr = container.AddComponent<MeshRenderer>();        
+
+        MeshFilter mf = container.GetComponent<MeshFilter>();
+        if(!mf)
+            mf = container.AddComponent<MeshFilter>();
+
+        mf.sharedMesh = new Mesh();
+        mf.sharedMesh.CombineMeshes(combine);
+    }
+}
---
+++ Tools/Assets/BuildMesh/PolyColliderToMesh.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class PolyColliderToMesh : MonoBehaviour
+{
+    [SerializeField]
+    private bool buildAtStart = true;
+
+    public void Start()
+    {
+        if(buildAtStart)
+            BuildMesh.Build(gameObject, GetComponent<PolygonCollider2D>());
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PolyColliderToMesh))]
+public class PointsToMeshEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PolyColliderToMesh go = target as PolyColliderToMesh;
+
+        if (GUILayout.Button("Build Mesh"))
+        {
+            BuildMesh.Build(go.gameObject, go.GetComponent<PolygonCollider2D>());
+        }
+    }
+}
+#endif
